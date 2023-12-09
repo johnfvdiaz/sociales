eff2f46cbe4e669227eb3f8b1dbd40b4ba52f54f
johnfvdiaz <153329297+johnfvdiaz@users.noreply.github.com>
2023-12-09 11:12:14 -0500

    Create botones.js

---
+++ botones.js
@@ -0,0 +1,9 @@
+function reproducirGif() {
+    var gif = document.getElementById("gif");
+    gif.src = "tu-imagen-animada.gif";
+}
+
+function pausarGif() {
+    var gif = document.getElementById("gif");
+    gif.src = "tu-imagen.gif";
+}
